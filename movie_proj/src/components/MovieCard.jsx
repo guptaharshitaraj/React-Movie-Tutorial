0cd9015b9fa21a7436ac875d1bdf5b56a27d841c
Harshita <guptaharshitaraj@gmail.com>
2026-03-22 12:20:16 +0530

    moviecard

---
+++ movie_proj/src/components/MovieCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MovieCard = ({movie}) => {
+    function onFavouriteClick() {
+        console.log('Favourite button clicked for movie:', movie.title);
+    }
+  return (
+    <div className='movie-card'>
+      <div className='movie-poster'>
+        <img src={movie.url} alt={movie.title} />
+            <div className='movie-overlay'>
+            <button className='favourite-btn' onClick={onFavouriteClick}>
+                
+            </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
