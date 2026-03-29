51434bdf457446e0dfa7a1e3673a29bf3be35f3d
Harshita <guptaharshitaraj@gmail.com>
2026-03-29 23:24:29 +0530

    navbar commit message

---
+++ movie_proj/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <nav>
+         <div>
+      <Link/>
+    </div></nav>
+   
+  )
+}
+
+export default Navbar
