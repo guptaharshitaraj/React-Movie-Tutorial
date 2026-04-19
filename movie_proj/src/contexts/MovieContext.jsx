19db4b927bc61e05dc16a3286d75b0bcc819bd19
Harshita <guptaharshitaraj@gmail.com>
2026-04-19 23:24:30 +0530

    context

--- movie_proj/src/contexts/MovieContext.jsx
+++ movie_proj/src/contexts/MovieContext.jsx
@@ -1,3 +1,12 @@
 import { createContext,useContext,useEffect } from "react";
 
 const MovieContext=createContext();
+
+export const usemovieContext=()=>useContext(MovieContext);
+
+export const MovieProvider=({children})=>{
+    
+    return<MovieContext.Provider >
+        {children}
+    </MovieContext.Provider>
+}
